a7df74cae501e87197e888f370b6e64be25c721d
yuchiba0121 <yuchiba0121@gmail.com>
2018-02-26 18:00:15 +0900

    s

--- furiya/colection/js/script.js
+++ furiya/colection/js/script.js
@@ -17,8 +17,30 @@ $(function() {
         // OUTOTU
         $('.outotu1 img').css('transform', 'translateY('+ scrollNum * -0.2 + 'px )');
 
+        $('.outotu2 img').css('transform', 'translateY('+ scrollNum * -0.3 + 'px )');
+
+        $('.o p').css('transform', 'translateY('+ scrollNum * -0.3 + 'px )');
+
+        // TAIYOU
+        $('.taiyou1 img').css('transform', 'translateY('+ scrollNum * -0.1 + 'px )');
+
+        $('.ty p').css('transform', 'translateY('+ scrollNum * -0.1 + 'px )');
+
+        // MIZUSHIBUKI
+        $('.mizushibuki1 img').css('transform', 'translateY('+ scrollNum * -0.2 + 'px )');
+
+        $('.msbk p').css('transform', 'translateY('+ scrollNum * -0.2 + 'px )');
+
+
+
         }
 
     });
+
+//     // 写真・コピー出てくるタイミングをこちらで。
+//     $('.taiyou1').on('inview', function() {
+//         // 要素がウィンドウの表示領域に現れたときに実行する処理
+//         $('.taiyou1').addClass('fade-in');
+// });
 
 });
