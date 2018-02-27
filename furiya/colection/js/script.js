0236447766b1a4d69d775f77df105e0d89e7278d
yuchiba0121 <yuchiba0121@gmail.com>
2018-02-27 22:08:29 +0900

    sz

--- furiya/colection/js/script.js
+++ furiya/colection/js/script.js
@@ -44,10 +44,49 @@ $(function() {
 
     });
 
-//     // 写真・コピー出てくるタイミングをこちらで。
-//     $('.taiyou1').on('inview', function() {
-//         // 要素がウィンドウの表示領域に現れたときに実行する処理
-//         $('.taiyou1').addClass('fade-in');
-// });
+    // 写真・コピー出てくるタイミングをこちらで。
+    $('#umi_zinwari').on('inview', function() {
+        // 要素がウィンドウの表示領域に現れたときに実行する処理
+        $('.chaku_umi img').addClass('fade-in').addClass('fade-in-delay2');
+        $('.umi h1').addClass('fade-in').addClass('fade-in-delay8');
+        $('.umi h2').addClass('fade-in').addClass('fade-in-delay12');
+        $('.sentens').addClass('fade-in').addClass('fade-in-delay12');
+        });
+
+    ////////////////////
+    $('#outotu_zinwari').on('inview', function() {
+        // 要素がウィンドウの表示領域に現れたときに実行する処理
+        $('.chaku_outotu img').addClass('fade-in').addClass('fade-in-delay2');
+        $('.svg2').addClass('fade-in').addClass('fade-in-delay8');
+        $('.outotu h2').addClass('fade-in').addClass('fade-in-delay12');
+        $('.sentens2').addClass('fade-in').addClass('fade-in-delay12');
+        });
+
+    ////////////////////
+    $('#taiyou_zinwari').on('inview', function() {
+        // 要素がウィンドウの表示領域に現れたときに実行する処理
+        $('.chaku_taiyou img').addClass('fade-in').addClass('fade-in-delay2');
+        $('.taiyou h1').addClass('fade-in').addClass('fade-in-delay8');
+        $('.taiyou h2').addClass('fade-in').addClass('fade-in-delay12');
+        $('.sentens3').addClass('fade-in').addClass('fade-in-delay12');
+        });
+
+    ////////////////////
+    $('#aoiroakairo_zinwari').on('inview', function() {
+        // 要素がウィンドウの表示領域に現れたときに実行する処理
+        $('.chaku_aoaka img').addClass('fade-in').addClass('fade-in-delay2');
+        $('.aoiroakairo h1').addClass('fade-in').addClass('fade-in-delay8');
+        $('.aoiroakairo h2').addClass('fade-in').addClass('fade-in-delay12');
+        $('.sentens4').addClass('fade-in').addClass('fade-in-delay12');
+        });
+
+    ////////////////////
+    $('#mizushibuki_zinwari').on('inview', function() {
+        // 要素がウィンドウの表示領域に現れたときに実行する処理
+        $('.chaku_mizushibuki img').addClass('fade-in').addClass('fade-in-delay2');
+        $('.mizushibuki h1').addClass('fade-in').addClass('fade-in-delay8');
+        $('.mizushibuki h2').addClass('fade-in').addClass('fade-in-delay12');
+        $('.sentens5').addClass('fade-in').addClass('fade-in-delay12');
+        });
 
 });
