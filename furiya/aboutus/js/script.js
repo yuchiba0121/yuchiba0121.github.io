75d1da4765699b34a12b32bfe3f6d9cc81b75128
yuchiba0121 <yuchiba0121@gmail.com>
2018-02-27 15:18:41 +0900

    s

--- furiya/aboutus/js/script.js
+++ furiya/aboutus/js/script.js
@@ -12,21 +12,3 @@ $(function() {
         }
     })
 });
-
-// // スクロール800pxでロゴを消す
-// $(function() {
-//
-//     //スクロールイベント
-//     $(window).scroll(function(){
-//
-//     // スクロール値を保存
-//     var scrollNum = $(this).scrollTop();
-//     console.log(scrollNum);
-//
-//     if(scrollNum > 900){
-//             $('.logo_furiya').addClass('in');
-//         } else {
-//             $('.logo_furiya').removeClass('in');
-//         }
-//     })
-// });
