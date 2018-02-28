30ec197d3dc653fb7d9425efb066a8789415fdfc
yuchiba0121 <yuchiba0121@gmail.com>
2018-02-28 18:52:49 +0900

    z

--- furiya/aboutus/js/script.js
+++ furiya/aboutus/js/script.js
@@ -8,7 +8,7 @@ $(function() {
     console.log(scrollNum);
         {
         // TOP
-        $('.top img').css('transform', 'translateY('+ scrollNum * -0.2 + 'px )');
+        $('.topPic').css('transform', 'translateY('+ scrollNum * -0.2 + 'px )');
 
         $('.coral').css('transform', 'translateY('+ scrollNum * -0.2 + 'px )');
 
@@ -19,17 +19,6 @@ $(function() {
         $('.turquoise').css('transform', 'translateY('+ scrollNum * -0.2 + 'px )');
         }
 
-
-
-    // // 写真・コピー出てくるタイミングをこちらで。
-    // $('#top').on('inview', function() {
-    //     $('h1').addClass('fade-in').addClass('fade-in-delay15');
-    //     $('.shape1').addClass('fade-in').addClass('fade-in-delay15');
-    //     $('.sentens1').addClass('fade-in').addClass('fade-in-delay20');
-    //     $('.sentens2').addClass('fade-in').addClass('fade-in-delay20');
-    //     $('.sentens3').addClass('fade-in').addClass('fade-in-delay25');
-    //     $('.topPic').addClass('fade-in').addClass('fade-in-delay10');
-    // });
 });
 
 });
