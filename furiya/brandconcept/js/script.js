4ce554fc006ac8f5be78d09645cd1077ed5d1fc6
yuchiba0121 <yuchiba0121@gmail.com>
2018-03-02 18:33:47 +0900

    s

--- furiya/brandconcept/js/script.js
+++ furiya/brandconcept/js/script.js
@@ -17,20 +17,19 @@ $(function() {
         $('.labra').css('transform', 'translateY('+ scrollNum * -0.2 + 'px )');
 
         $('.turquoise').css('transform', 'translateY('+ scrollNum * -0.2 + 'px )');
-        }
 
         $('#bg1').css('transform', 'translateY('+ scrollNum * -1.0 + 'px )');
         }
 
 });
 
-////////////////////
-$('#stone_zinwari').on('inview', function() {
-    // 要素がウィンドウの表示領域に現れたときに実行する処理
-    $('.coral').addClass('fade-in').addClass('fade-in-delay10');
-    $('.sunstone').addClass('fade-in').addClass('fade-in-delay14');
-    $('.labra').addClass('fade-in').addClass('fade-in-delay18');
-    $('.turquoise').addClass('fade-in').addClass('fade-in-delay22');
-    });
+    ////////////////////
+    $('#stone_zinwari').on('inview', function() {
+        // 要素がウィンドウの表示領域に現れたときに実行する処理
+        $('.coral').addClass('fade-in').addClass('fade-in-delay10');
+        $('.sunstone').addClass('fade-in').addClass('fade-in-delay14');
+        $('.labra').addClass('fade-in').addClass('fade-in-delay18');
+        $('.turquoise').addClass('fade-in').addClass('fade-in-delay22');
+        });
 
 });
